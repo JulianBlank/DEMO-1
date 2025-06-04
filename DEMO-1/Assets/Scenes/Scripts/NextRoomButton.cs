653eab5d8ce52aa1f2e09dad41a5fe6e5c726a18
PurePerfektion1 <pureperfection@outlook.de>
2025-06-04 08:37:32 +0200

    added multiple room function
    ligma ass

--- DEMO-1/Assets/Scenes/Scripts/NextRoomButton.cs
+++ DEMO-1/Assets/Scenes/Scripts/NextRoomButton.cs
@@ -2,33 +2,17 @@ using UnityEngine;
 
 public class RoomProgressButton : MonoBehaviour
 {
-    public GameObject doorToDestroy;
-    public GameObject[] spawnPointsToActivate;
-    public DungeonRoomManager roomManager; // <--- NEU: Referenz auf den RoomManager
+    public DungeonRoomManager roomManager;
 
     public void OnButtonClick()
     {
-        Debug.Log("Button gedrückt!");
-
-        if (doorToDestroy != null)
-        {
-            Destroy(doorToDestroy);
-            Debug.Log("Tür zerstört!");
-        }
-
-        foreach (GameObject obj in spawnPointsToActivate)
-        {
-            if (obj != null)
-                obj.SetActive(true);
-        }
-
         if (roomManager != null)
         {
-            roomManager.ProgressToNextRoom(); // <--- Gegner an neuen Punkten spawnen
+            roomManager.OnRoomProgressButtonClicked();
         }
         else
         {
-            Debug.LogWarning("RoomManager fehlt im Button!");
+            Debug.LogWarning("RoomManager nicht zugewiesen im Button!");
         }
     }
 }
