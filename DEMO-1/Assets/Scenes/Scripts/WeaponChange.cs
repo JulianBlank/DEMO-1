c5a08a3282575cfcff5fdc50b61acd2692f6cb78
JulianBlank <julian_blank@outlook.de>
2025-07-02 08:44:07 +0200

    Update WeaponChange.cs

--- DEMO-1/Assets/Scenes/Scripts/WeaponChange.cs
+++ DEMO-1/Assets/Scenes/Scripts/WeaponChange.cs
@@ -16,7 +16,7 @@ public class AttachOnProximity : MonoBehaviour
 
         if (objectRenderer != null && objectRenderer.material.HasProperty("_MainTex"))
         {
-            originalTexture = objectRenderer.material.schwert;
+            originalTexture = objectRenderer.material.originalTexture;
         }
         else
         {
