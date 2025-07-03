f9a287e71c5efbf45e56ac9327523fa889b87745
JulianBlank <julian_blank@outlook.de>
2025-07-03 21:02:16 +0200

    Unity my balls
    I love tags

--- DEMO-1/Assets/Scripts/Player/DungeonPlayerController.cs
+++ DEMO-1/Assets/Scripts/Player/DungeonPlayerController.cs
@@ -5,31 +5,46 @@ using System.Collections;
 public class DungeonPlayerController : MonoBehaviour
 {
     [Header("Bewegung")]
-    [SerializeField] private float moveSpeed = 50f;
-    private Vector3 target;
-    private Vector3 lastMoveDirection = Vector3.right;
+    [SerializeField] private float moveSpeed = 50f;     //Bewegungsgeschwindigkeit
+    private Vector3 target;     //Zu schädigender Gegner
+    private Vector3 lastMoveDirection = Vector3.right;  
 
     [Header("Angriff")]
-    public float attackRange = 1f;
-    public Vector2 attackBoxSize = new Vector2(1f, 1f);
-    public LayerMask enemyLayer;
-    public int damage = 5;
-    public float attackCooldown = 1f;
-    private bool canAttack = true;
+    public float attackRange = 1f;  //Entfernung des Spielers vom Mittelpunkt der AttackBox
+    public Vector2 attackBoxSize = new Vector2(1f, 1f);     //Grösse der Attackbox
+    public LayerMask enemyLayer;    // Maske, um Enemies zu kennzeichnen / Legt das Schadenslayer fest
+    public int damage = 5;      //Schaden
+    public float attackCooldown = 1f;   //Cooldown bis zur nächsten Attacke
+    private bool canAttack = true;  
 
     [SerializeField] private AttachOnProximity wpn;
+    private SpriteRuntimeEditor spriteChanger;
+
+    [Header("Sprites")]
     [SerializeField] private Sprite north;
     [SerializeField] private Sprite south;
     [SerializeField] private Sprite west;
     [SerializeField] private Sprite east;
-    [SerializeField] private Sprite holdingWeapon;
-
-    private SpriteRuntimeEditor spriteChanger;
+    [SerializeField] private Sprite holdingSword_north;
+    [SerializeField] private Sprite holdingSword_south;
+    [SerializeField] private Sprite holdingSword_west;
+    [SerializeField] private Sprite holdingSword_east;
 
     private void Start()
     {
         target = transform.position;
         spriteChanger = GetComponent<SpriteRuntimeEditor>();
+        if (wpn == null)
+        {
+            GameObject weaponGO = GameObject.FindGameObjectWithTag("Weapon");
+            if (weaponGO != null)
+            {
+                wpn = weaponGO.GetComponent<AttachOnProximity>();
+            }
+        }
+
+        if (wpn == null)
+            Debug.LogError("Waffe konnte nicht gefunden werden!");
     }
 
     private void Update()
@@ -48,7 +63,7 @@ public class DungeonPlayerController : MonoBehaviour
         {
             lastMoveDirection = moveDirection;
 
-            if (Mathf.Abs(moveDirection.x) > Mathf.Abs(moveDirection.y))
+            if (Mathf.Abs(moveDirection.x) > Mathf.Abs(moveDirection.y) && wpn.GetStatus() == false && wpn != null) // Controls player sprite without a weapon
             {
                 if (moveDirection.x > 0)
                     spriteChanger.ChangeSprite(west);
@@ -63,10 +78,21 @@ public class DungeonPlayerController : MonoBehaviour
                     spriteChanger.ChangeSprite(south);
             }
 
-            if (wpn != null && wpn.GetStatus())
+            if (Mathf.Abs(moveDirection.x) > Mathf.Abs(moveDirection.y) && wpn.GetStatus() && wpn != null) // Controls player sprite with a weapon
             {
-                spriteChanger.ChangeSprite(holdingWeapon);
+                if (moveDirection.x > 0)
+                    spriteChanger.ChangeSprite(holdingSword_west);
+                else
+                    spriteChanger.ChangeSprite(holdingSword_east);
             }
+            else
+            {
+                if (moveDirection.y > 0)
+                    spriteChanger.ChangeSprite(holdingSword_north);
+                else
+                    spriteChanger.ChangeSprite(holdingSword_south);
+            }
+
         }
 
         transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
